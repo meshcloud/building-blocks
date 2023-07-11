705613d807a205de4142356f59efd58d8cde2660
MJ <mkazemi@meshcloud.io>
2023-07-11 16:11:33 +0200

    cosmosDB backend

--- azure/terraform/cosmosdb/autoscale-throughput/backend.tf
+++ azure/terraform/cosmosdb/autoscale-throughput/backend.tf
@@ -4,8 +4,8 @@ terraform {
     # *** To do so, Add these keys as "Input Variables" and choose "Environment" in "Provide as" field 
 
     # information of the storage account and the blob holding the terraform state file
-    tenant_id            = "tenant-id-state"
-    subscription_id      = "subscription-id-state"
+    tenant_id            = "703c8d27-13e0-4836-8b2e-8390c588cf80"
+    subscription_id      = "0688c9ba-193b-49f3-a3a5-98735a3908df"
     resource_group_name  = "cloudfoundation-tfstates"
     storage_account_name = "tfstatesbuildingblocks"
     container_name       = "tfstates"
