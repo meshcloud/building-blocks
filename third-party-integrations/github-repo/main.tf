c20f78554b247ffcbf705c0bb198eff049bee7c6
MJ <mkazemi@meshcloud.io>
2023-08-14 23:27:13 +0200

    add vulnerability alert github repo

--- third-party-integrations/github-repo/main.tf
+++ third-party-integrations/github-repo/main.tf
@@ -1,8 +1,9 @@
 resource "github_repository" "repository" {
   count = var.create_new ? 1 : 0
 
-  name        = var.repo_name
-  description = var.description
-  visibility  = var.visibility
-  auto_init   = false
+  name                 = var.repo_name
+  description          = var.description
+  visibility           = var.visibility
+  auto_init            = false
+  vulnerability_alerts = true
 }
