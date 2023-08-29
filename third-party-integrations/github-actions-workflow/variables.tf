c1019b142ee2b4659a69f790afc3ba487d92952f
MJ <mkazemi@meshcloud.io>
2023-08-29 17:22:40 +0200

    add github actions file

---
+++ third-party-integrations/github-actions-workflow/variables.tf
@@ -0,0 +1,28 @@
+variable "repository_url" {
+  type        = string
+  description = "The url to the target repository"
+}
+
+variable "branch" {
+  type = string
+  description = "branch name"
+  default = "main"
+}
+
+variable "commit_message" {
+  type        = string
+  description = "Commit message when adding or updating the managed file."
+  default     = "Managed by Terraform"
+}
+
+variable "commit_author" {
+  type        = string
+  description = "Committer author name to use"
+  default     = "Terraform User"
+}
+
+variable "commit_email" {
+  type        = string
+  description = "Committer email address to use"
+  default     = "terraform@example.com"
+}
