1b9f27f6b9860ec4635446d350d0616fab146321
MJ <mkazemi@meshcloud.io>
2023-08-30 15:22:15 +0200

    feat: add readme file

--- other-providers/sap/subaccount/variables.tf
+++ other-providers/sap/subaccount/variables.tf
@@ -1,11 +1,12 @@
 variable "globalaccount" {
   type        = string
-  description = "The subdomain of the global account in which you want to manage resources. To be found in the cockpit, in the global account view."
+  description = "The subdomain of the global account in which you want to manage resources. To be found in the cockpit, in the global account view. Should look like '98d249c7trial-ga'"
 }
 
 variable "parent_id" {
   type        = string
   description = "ID of the account where this subaccount is going to created in, use global if there is no parent account"
+  default     = ""
 }
 
 variable "subaccount_name" {
@@ -21,12 +22,33 @@ variable "subaccount_subdomain" {
 variable "subaccount_region" {
   type        = string
   description = "The region in which the subaccount was created."
+  default     = "us10"
 }
 
-variable "environmentinstance_name" {
-  type = string
+variable "environment_instance_name" {
+  type        = string
+  description = "The name of the environment instance"
 }
 
 variable "instance_name" {
-  type = string
+  type        = string
+  description = "Part of Parameters which is The configuration for the environment instance. Should be unique"
+}
+
+variable "environment_type" {
+  type        = string
+  description = "The type of the environment instance that is used"
+  default     = "cloudfoundry"
+}
+
+variable "service_name" {
+  type        = string
+  description = "The name of the service for the environment instance in the corresponding service broker's catalog"
+  default     = "cloudfoundry"
+}
+
+variable "plan_name" {
+  type        = string
+  description = "The name of the service plan for the environment instance in the corresponding service broker's catalog. e.g. 'standard'"
+  default     = "standard"
 }
