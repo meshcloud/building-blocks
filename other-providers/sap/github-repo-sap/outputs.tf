2307ebffa514de33ee5a220be3990759ab9adef1
MJ <mkazemi@meshcloud.io>
2023-09-04 16:00:27 +0200

    feat: edit outputs sap-btp github

--- other-providers/sap/github-repo-sap/outputs.tf
+++ other-providers/sap/github-repo-sap/outputs.tf
@@ -1,15 +1,15 @@
 output "repo_name" {
-  value = var.create_new ? github_repository.repository.name : var.repo_name
+  value = var.create_workflow ? github_repository.repository.name : var.repo_name
 }
 
 output "repo_full_name" {
-  value = var.create_new ? github_repository.repository.full_name : "${var.github_owner}/${var.repo_name}"
+  value = var.create_workflow ? github_repository.repository.full_name : "${var.github_owner}/${var.repo_name}"
 }
 
 output "repo_html_url" {
-  value = var.create_new ? github_repository.repository.html_url : "https://github.com/${var.github_owner}/${var.repo_name}"
+  value = var.create_workflow ? github_repository.repository.html_url : "https://github.com/${var.github_owner}/${var.repo_name}"
 }
 
 output "repo_git_clone_url" {
-  value = var.create_new ? github_repository.repository.git_clone_url : "https://github.com/${var.github_owner}/${var.repo_name}"
+  value = var.create_workflow ? github_repository.repository.git_clone_url : "https://github.com/${var.github_owner}/${var.repo_name}"
 }
