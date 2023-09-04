c37c500f7e6ae03a519ba7b2200c3a68ea985176
MJ <mkazemi@meshcloud.io>
2023-09-04 15:17:12 +0200

    feat: edit readme and variables

--- other-providers/sap/github-repo-sap/provider.tf
+++ other-providers/sap/github-repo-sap/provider.tf
@@ -11,5 +11,5 @@ terraform {
 provider "github" {
 # token = ""      # Set the GITHUB_TOKEN environment variable 
 
-owner   = var.github_owner
+owner   = var.github_owner # This is the target GitHub organization or individual user account to manage
 }
