71fcedcd847196212b287c822ce3c72547649ab8
Johannes Rudolph <jrudolph@meshcloud.io>
2023-10-05 11:29:38 +0200

    feat: add route53-dns-alias-record building block

---
+++ aws/terraform/networking-contentdelivery/route53-dns-alias-record/main.tf
@@ -0,0 +1,16 @@
+data "aws_route53_zone" "zone" {
+  name         = var.zone_name
+  private_zone = var.private_zone
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = join(".", compact([var.sub, data.aws_route53_zone.zone.name]))
+  type    = var.type
+
+  alias {
+    name                   = var.alias_name
+    evaluate_target_health = var.evaluate_target_health
+    zone_id                = var.alias_zone_id
+  }  
+}
