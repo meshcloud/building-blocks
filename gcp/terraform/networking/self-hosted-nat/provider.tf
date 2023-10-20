ceb59e076eb2ea603a7dc78b21aa461a3e54643f
Mohammad Alhussan <malhussan@meshcloud.io>
2023-10-20 10:00:29 +0200

    doc: add README for nat BB

--- gcp/terraform/networking/self-hosted-nat/provider.tf
+++ gcp/terraform/networking/self-hosted-nat/provider.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "gcs" {
-    bucket      = "terraform-state-nat-bb"
-    prefix      = "nat"
-    credentials = "./credentials.json"
-  }
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -11,10 +6,4 @@ terraform {
     }
   }
   required_version = ">= 0.13"
-}
-
-provider "google" {
-  project     = var.project
-  zone        = var.zone
-  credentials = "./credentials.json"
 }
