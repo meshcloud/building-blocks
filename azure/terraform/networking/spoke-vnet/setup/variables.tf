985c6b9445b7aa5dc98139fc27f4479a4a9cf976
MJ <mkazemi@meshcloud.io>
2023-11-14 17:45:12 +0100

    feat/edit-azure-vnet-spoke

--- azure/terraform/networking/spoke-vnet/setup/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-  type    = string
-  default = "westeurope"
-}
-
-variable "subscription_id" {
-  type        = string
-  description = "Subscription ID for deployment of the resources"
-}
