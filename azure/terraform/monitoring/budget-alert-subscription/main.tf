e3d9096a9ade36482ff6be8a22b2daf40a4f68c7
MJ <mkazemi@meshcloud.io>
2023-11-21 11:59:32 +0100

    feat: Azure budget alert for subscriptions

---
+++ azure/terraform/monitoring/budget-alert-subscription/main.tf
@@ -0,0 +1,73 @@
+
+//-----------------------------------------------
+// Budget on subscription level
+//-----------------------------------------------
+data "azurerm_subscription" "subscription" {
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+}
+
+resource "azurerm_consumption_budget_subscription" "subscription_budget" {
+  name            = var.budget_name
+  subscription_id = data.azurerm_subscription.subscription.id
+
+  amount     = 1000
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = var.start_date #"2022-06-01T00:00:00Z"
+    end_date   = var.end_date   #"2022-07-01T00:00:00Z"
+  }
+
+  # filter {
+  #   dimension {
+  #     name = "ResourceGroupName"
+  #     values = [
+  #       azurerm_resource_group.example.name,
+  #     ]
+  #   }
+
+  #   tag {
+  #     name = "foo"
+  #     values = [
+  #       "bar",
+  #       "baz",
+  #     ]
+  #   }
+  # }
+
+  notification {
+    enabled   = true
+    threshold = 90.0
+    operator  = "EqualTo"
+
+    contact_emails = var.contact_emails
+
+  }
+
+  notification {
+    enabled        = false
+    threshold      = 100.0
+    operator       = "GreaterThan"
+    threshold_type = "Forecasted"
+
+    contact_emails = var.contact_emails
+
+    contact_groups = [
+      azurerm_monitor_action_group.action_group.id,
+    ]
+
+  }
+
+}
+//-----------------------------------------------
+// Action Group
+//-----------------------------------------------
+resource "azurerm_resource_group" "rg_action_group" {
+  name = var.resource_group_for_action_group_name
+  location = var.location
+}
+resource "azurerm_monitor_action_group" "action_group" {
+  name                = "budget_action_group"
+  resource_group_name = azurerm_resource_group.rg_action_group.name
+  short_name          = "bdgt_ag"
+}
---
+++ azure/terraform/monitoring/budget-alert-subscription/variables.tf
@@ -0,0 +1,41 @@
+
+variable "location" {
+  type = string
+  default = "westeurope"
+}
+
+variable "budget_name" {
+  type = string
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  type = string
+}
+
+variable "resource_group_for_action_group_name" {
+  type = string
+}
+
+variable "end_date" {
+  type        = string
+  description = "End date of the budget rule, Timestamp in the format YYYY-MM-DDTHH:MM:SSZ"
+  validation {
+    condition     = can(regex("^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T\\d{2}:\\d{2}:\\d{2}Z$", var.end_date))
+    error_message = "Timestamp must be in the format YYYY-MM-DDTHH:MM:SSZ"
+  }
+}
+
+variable "start_date" {
+  type        = string
+  description = "Start date of the budget rule, should be first day of the month"
+  validation {
+    condition     = can(regex("^\\d{4}-(0[1-9]|1[0-2])-(01)T\\d{2}:\\d{2}:\\d{2}Z$", var.start_date))
+    error_message = "The 'start_date' must be in the format 'YYYY-MM-01THH:MM:SSZ', e.g., '2023-11-01T00:00:00Z'."
+  }
+}
+
+variable "contact_emails" {
+  type = list(string)
+description = "List of emails of the users who should receive the Budget alert. e.g. ['foo@example.com', 'bar@example.com']"
+}
+
