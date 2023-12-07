276713a1647a8634707e9c4369f0fb357499a284
MJ <mkazemi@meshcloud.io>
2023-12-07 14:22:30 +0100

    fix: add action group for budget alert

--- azure/terraform/monitoring/budget-alert-subscription/outputs.tf
+++ azure/terraform/monitoring/budget-alert-subscription/outputs.tf
@@ -13,3 +13,11 @@ output "end_date" {
 output "budget_amount" {
   value = azurerm_consumption_budget_subscription.subscription_budget.amount
 }
+
+output "action_group_id" {
+  value = azurerm_monitor_action_group.action_group.id
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.action_group_rg.id
+}
