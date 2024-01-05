01208383cceef703bfb7d3f052a60bfd6a519b25
MJ <mkazemi@meshcloud.io>
2024-01-05 14:56:56 +0100

    feat: add outputs for budget-alert

--- gcp/terraform/operations/budget-alert/provider.tf
+++ gcp/terraform/operations/budget-alert/provider.tf
@@ -1,15 +1,18 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.8.0"
-    }
-  }
-}
+// Uncomment this section if you don't generate provider's configuration generated 
 
-provider "google" {
-  project               = var.projectid
-  billing_project       = var.projectid
-  user_project_override = true
-}
+
+# terraform {
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "5.8.0"
+#     }
+#   }
+# }
+
+# provider "google" {
+#   project               = var.projectid
+#   billing_project       = var.projectid
+#   user_project_override = true
+# }
 
