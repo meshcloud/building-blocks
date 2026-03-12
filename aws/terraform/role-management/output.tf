6f58112acbe823d32814f123b129ee74d80bd75e
Shamil Radzhabov <sradzhabov@meshcloud.io>
2026-03-12 17:10:00 +0100

    Enhanced output message

--- aws/terraform/role-management/output.tf
+++ aws/terraform/role-management/output.tf
@@ -2,8 +2,6 @@ output "summary" {
     description = "Summary of building block outputs in Markdown format."
     value = <<EOF
 
-# Role assignment building block
-
-**Approval**: ${var.approval == true ? "The role assignement request was approved" : "The role assignment request was rejected."}
+**Approval**: ${var.approval == true ? "The role assignement request was approved" : "The role assignment request was **rejected**."}
 EOF
 } 
